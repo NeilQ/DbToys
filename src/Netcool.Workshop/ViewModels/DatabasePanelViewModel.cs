fb17108151a6ac1027fde0cb7ce9a46904eb7af4
Chen <neil_chen_0918@live.cn>
2021-08-09 17:21:53 +0800

    add PostgreSql login view

--- src/Netcool.Workshop/ViewModels/DatabasePanelViewModel.cs
+++ src/Netcool.Workshop/ViewModels/DatabasePanelViewModel.cs
@@ -1,7 +1,10 @@
 ﻿using System.Reactive;
+using System.Windows;
 using DynamicData.Binding;
 using HandyControl.Controls;
+using HandyControl.Tools.Extension;
 using Netcool.Workshop.Database;
+using Netcool.Workshop.Views;
 using ReactiveUI;
 using ReactiveUI.Fody.Helpers;
 
@@ -10,7 +13,6 @@ namespace Netcool.Workshop.ViewModels
 {
     public class DatabasePanelViewModel : ReactiveObject
     {
-
         [Reactive]
         public IObservableCollection<TreeItem> ConnectionItems { get; set; } = new ObservableCollectionExtended<TreeItem>();
 
@@ -18,6 +20,8 @@ namespace Netcool.Workshop.ViewModels
         public TreeItem SelectedItem { get; set; }
 
         public ReactiveCommand<string, Unit> NewConnectionCommand { get; set; }
+
+        private PostgreSqlLoginViewModel _postgreSqlLoginViewModel = new PostgreSqlLoginViewModel();
 
         public DatabasePanelViewModel()
         {
@@ -28,6 +32,17 @@ namespace Netcool.Workshop.ViewModels
             NewConnectionCommand = ReactiveCommand.Create<string>((value) =>
             {
                 Growl.Info(value);
+
+                if (value == "PostgreSql")
+                {
+                    var window = new PostgreSqlLoginView
+                    {
+                        Owner = Application.Current.MainWindow,
+                        ViewModel = _postgreSqlLoginViewModel
+                    };
+                    _postgreSqlLoginViewModel.CloseAction = () => { window.Close(); };
+                    window.Show();
+                }
             });
         }
 
