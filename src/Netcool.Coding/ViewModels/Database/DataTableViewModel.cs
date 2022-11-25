4d9386177f9ce31fcefc8b29dd5a03d958addd74
Chen <neil_chen_0918@live.cn>
2022-11-25 17:15:55 +0800

    udpate layout

---
+++ src/Netcool.Coding/ViewModels/Database/DataTableViewModel.cs
@@ -0,0 +1,14 @@
+﻿using ReactiveUI;
+using Splat;
+
+namespace Netcool.Coding.ViewModels;
+
+public class DataTableViewModel : ReactiveObject, IRoutableViewModel
+{
+    public DataTableViewModel(IScreen screen = null)
+    {
+        HostScreen = screen ?? Locator.Current.GetService<IScreen>();
+    }
+    public string UrlPathSegment => "data-table";
+    public IScreen HostScreen { get; }
+}
--- src/Netcool.Coding/ViewModels/LayoutViewModel.cs
+++ src/Netcool.Coding/ViewModels/LayoutViewModel.cs
@@ -1,12 +1,16 @@
 ﻿using ReactiveUI;
+using Splat;
 
 namespace Netcool.Coding.ViewModels
 {
-    public class LayoutViewModel : ReactiveObject
+    public class LayoutViewModel : ReactiveObject,IScreen
     {
+        public RoutingState Router { get; }
 
         public LayoutViewModel()
         {
+            Router = new RoutingState();
+            Locator.CurrentMutable.RegisterConstant(this, typeof(IScreen));
         }
 
     }
